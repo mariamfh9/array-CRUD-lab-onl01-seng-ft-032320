fde1df430e00e922ad479188b19b554c34a3cd07
voracious-semicolon-2550 <mfh.haider@gmail.com>
2020-03-24 19:41:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my_animals = [cat, dog, bird, snake]
+  my_animals = ["cat", "dog", "bird", "snake"]
 end
 
 def add_element_to_end_of_array(array, element)
